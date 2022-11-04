04f44e02510024ace7ee1462ae9665acf300732e
Daniel Serezane <serezane@protonmail.com>
2022-11-04 18:16:47 -0300

    gramática: sei lá

--- projects/grammar/js/grammar.js
+++ projects/grammar/js/grammar.js
@@ -1,22 +1,37 @@
 const list = $('.list');
 const pattern = new RegExp("^[a-z]*[A-Z]?$");
-$('.add-rule').addEventListener('click', () => {
-    list.innerHTML += '</br><input type="text"> → <input type="text">';
+$('.add-rule').addEventListener('click', (e) => {
+    list.insertAdjacentHTML('beforeend', '</br><input type="text"> → <input type="text" class="rule" placeholder="λ" oninput="validateRule(this)">');
 })
 
 function validateRule(el) {
-    if (pattern.test(el.value)) {
+    const valid = pattern.test(el.value);
+    if (valid) {
         el.classList.remove('red');
-        return true;
     } else {
         el.classList.add('red');
-        return false;
     }
+    validateInput();
+    return valid;
+}
+
+const validateAllRules = () => {
+    const rules  = document.querySelectorAll('.rule')
+    let allValid = true;
+    for (let i = 0; i < rules.length; i++) {
+        const validRule = pattern.test(rules[i].value);
+        if (!validRule) {
+            rules[i].classList.add('red');
+            allValid = false;
+            break;
+        }
+    }
+    return allValid;
 }
 
 function validateInput() {
     // get all inputs with class 'rule'
-    const inputs = document.querySelectorAll('.rule');
-    const allValidInput = Array.from(inputs).every(validateRule);
-    console.log(allValidInput);
+    const input = $('.input');
+    const validRules = validateAllRules();
+    console.log(validRules);
 }
