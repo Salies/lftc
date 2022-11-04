fe2806b5e610e56ab0f3c5f3ec66668565c2c7b5
Daniel Serezane <serezane@protonmail.com>
2022-11-04 17:53:15 -0300

    sei lá

--- projects/grammar/js/grammar.js
+++ projects/grammar/js/grammar.js
@@ -1,5 +1,22 @@
-let lista = $('.lista');
-
-$('.add-regra').addEventListener('click', ()=>{
-    lista.innerHTML += '</br><input type="text"> → <input type="text">';
+const list = $('.list');
+const pattern = new RegExp("^[a-z]*[A-Z]?$");
+$('.add-rule').addEventListener('click', () => {
+    list.innerHTML += '</br><input type="text"> → <input type="text">';
 })
+
+function validateRule(el) {
+    if (pattern.test(el.value)) {
+        el.classList.remove('red');
+        return true;
+    } else {
+        el.classList.add('red');
+        return false;
+    }
+}
+
+function validateInput() {
+    // get all inputs with class 'rule'
+    const inputs = document.querySelectorAll('.rule');
+    const allValidInput = Array.from(inputs).every(validateRule);
+    console.log(allValidInput);
+}
