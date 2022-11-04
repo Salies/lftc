4ba90a7c488a2bc973b9104d2b142838f85c7983
Becelli <gustavobecelli@gmail.com>
2022-11-04 19:01:11 -0300

    wip: basic funcional UI

--- projects/grammar/js/grammar.js
+++ projects/grammar/js/grammar.js
@@ -1,37 +1,43 @@
-const list = $('.list');
+const list = $(".list");
 const pattern = new RegExp("^[a-z]*[A-Z]?$");
-$('.add-rule').addEventListener('click', (e) => {
-    list.insertAdjacentHTML('beforeend', '</br><input type="text"> → <input type="text" class="rule" placeholder="λ" oninput="validateRule(this)">');
-})
 
-function validateRule(el) {
-    const valid = pattern.test(el.value);
-    if (valid) {
-        el.classList.remove('red');
-    } else {
-        el.classList.add('red');
-    }
-    validateInput();
-    return valid;
-}
+$(".add-rule").addEventListener("click", (e) => {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<div class="max-w-lg p-0.5 mx-auto">
+    <input type="text" value="S" class="w-16 bg-gray-200 rounded-lg text-center" />
+    <span>→</span>
+    <input type="text" placeholder="λ" oninput="validateRule(this)" class="rule w-fit bg-gray-200 rounded-lg text-center" />
+  </div>`
+  );
+});
+
+const validateRule = (el) => {
+  const valid = pattern.test(el.value);
+  if (valid) el.classList.remove("bg-red-300");
+  else el.classList.add("bg-red-300");
+
+  validateInput();
+  return valid;
+};
 
 const validateAllRules = () => {
-    const rules  = document.querySelectorAll('.rule')
-    let allValid = true;
-    for (let i = 0; i < rules.length; i++) {
-        const validRule = pattern.test(rules[i].value);
-        if (!validRule) {
-            rules[i].classList.add('red');
-            allValid = false;
-            break;
-        }
+  const rules = document.querySelectorAll(".rule");
+  let allValid = true;
+  for (let rule of rules) {
+    const isValidRule = pattern.test(rule.value);
+    if (!isValidRule) {
+      allValid = false;
+      break;
     }
-    return allValid;
-}
+  }
+  return allValid;
+};
 
-function validateInput() {
-    // get all inputs with class 'rule'
-    const input = $('.input');
-    const validRules = validateAllRules();
-    console.log(validRules);
-}
+const validateInput = () => {
+  const { value: grammar } = $(".input-grammar");
+  const areRulesValid = validateAllRules();
+  console.log("areRulesValid", areRulesValid);
+  console.log("grammar", grammar);
+  // Todo continue here.
+};
