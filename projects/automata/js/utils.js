654a685cd6f9540ff1732c4b572a5a3c365dd56c
Daniel Serezane <serezane@protonmail.com>
2023-01-03 21:47:38 -0300

    autômato: estilo melhor

--- projects/automata/js/utils.js
+++ projects/automata/js/utils.js
@@ -3,5 +3,5 @@ const $ = document.querySelector.bind(document);
 const warningsDiv = $('.warnings');
 
 const setWarning = (msg) => {
-    swal("Erro!", msg, "error");
+    swal("Erro", msg, "error");
 }
